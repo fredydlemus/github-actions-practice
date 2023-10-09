3a9a2d1eda0faedfca8495b018da8c8be4d42453
fredydlemus <fflores@eaglehubs.com>
2023-10-08 19:56:06 -0600

    fix workflow

--- hola_lenguaje.py
+++ hola_lenguaje.py
@@ -3,7 +3,7 @@ import os
 def main():
     nombre = os.getenv("USERNAME")
     print(f"Hola {nombre} desde Github!")
-    languaje = os.getenv("FAVORITE_LANGUAGE")
+    languaje = os.getenv("FAV_LANG")
     print(f"Tu lenguaje favorito es {languaje}!")
 
 if __name__ == "__main__":
